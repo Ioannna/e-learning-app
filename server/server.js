ee09455ce5db81127b080b870c7a228e3dd3cbcb
Ioannna <ioanabiiris@gmail.com>
2022-08-23 20:25:12 +0300

    update23August

--- server/server.js
+++ server/server.js
@@ -21,8 +21,29 @@ app.get("/", (req, res) => {
     console.log(`Server started on port ${port}`);
   });
 
+  let lessonsFirstSemester = ["Lessoooon1", "Lesson2", "Lesson2"]
+  let lessonsSecondSemester = ["2L1", "2L2", "2L3"]
+
   io.on("connection", (socket) => {
       console.log(`[SOCKET CONNECTED] ${socket.id}`)
 
-      
+      socket.emit("connected", socket.id)
+
+      socket.on("enter-class", (className) => {
+
+          socket.emit("data", className)
+      })
+
+      socket.on("first-semester", (name) => {
+          socket.emit("lessons-first-semester", lessonsFirstSemester)
+      })
+
+      socket.on("second-semester", (name) => {
+        socket.emit("lessons-second-semester", lessonsSecondSemester)
+    })
+
+    socket.on("display-lesson", (name) => {
+        socket.emit("display-lesson1", lessonsFirstSemester[0])
+        console.log("received display lesson")
+    })
 })
